463d3f81475a3459ab3ca81a52ce69c91946e187
avimitin <avimitin@gmail.com>
2020-10-15 14:52:42 +0800

    add print args selection for more simple output

--- Algorithms/Sort/AMain.c
+++ Algorithms/Sort/AMain.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void Show(int len, int *a);
 void QuickSort(int length, int *array);
@@ -14,12 +15,22 @@ int main(int argc, char *argv[]) {
 
     // Parse args to int array;
     for (int i = 0; i < argc-1; i++) {
-        a[i] = *argv[i+1]-48;
+        a[i] = atoi(argv[i+1]);
     }
 
     // Print user's input;
-    printf("Args you input: ");
-    Show(argc-1, a);
+    printf("Do you want to print the args? [Y/n]");
+    char NeedPrint;
+    scanf("%c", &NeedPrint);
+    if (NeedPrint == 'Y' || NeedPrint == 'y') {
+        printf("Args you input: ");
+        Show(argc-1, a);
+    } 
+    else if (NeedPrint == 'n' || NeedPrint == 'N') { } 
+    else {
+        printf("Invalid input!\n");
+        exit(-1);
+    }
     
     // Print input after sort;
     QuickSort(argc-1, a);
