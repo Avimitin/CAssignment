4efc8bd4a396f42a68d5bd4e3f2e550338efdf50
avimitin <avimitin@gmail.com>
2020-10-24 12:48:55 +0800

    fix wrong alg

--- Algorithms/Search/SymbolTable/BSST.c
+++ Algorithms/Search/SymbolTable/BSST.c
@@ -3,18 +3,13 @@
 #include <string.h>
 
 // Array that contain other pointers.
-KeyType **keys;
-ValType **vals;
+KeyType *keys;
+ValType *vals;
 int N;
 
 void Init(int cap) {
-	keys = (KeyType**)calloc(cap, sizeof(KeyType));
-	vals = (ValType**)calloc(cap, sizeof(ValType));
-	for (int i = 0; i < cap; i++) {
-		keys[i] = (KeyType*)calloc(1, sizeof(KeyType));
-		keys[i] -> KeyVal = (char*)calloc(50, sizeof(char));
-		vals[i] = (ValType*)calloc(1, sizeof(ValType));
-	}
+	keys = (KeyType*)calloc(cap, sizeof(KeyType));
+	vals = (ValType*)calloc(cap, sizeof(ValType));
 }
 
 int size() {
@@ -25,9 +20,9 @@ int Rank(char * key) {
 	int lo = 0, hi = N - 1;
 	while(lo <= hi) {
 		int mid = lo + (hi - lo)/2;
-		if (strcmp(keys[mid]->KeyVal, key) < 0) {
+		if (strcmp(keys[mid].KeyVal, key) < 0) {
 			lo = mid + 1;
-		} else if (strcmp(keys[mid]->KeyVal, key) > 0) {
+		} else if (strcmp(keys[mid].KeyVal, key) > 0) {
 			hi = mid - 1;
 		} else {
 			return mid;
@@ -38,31 +33,31 @@ int Rank(char * key) {
 
 ValType * Get(char * key) {
 	int i = Rank(key);
-	if (i <= N && strcmp(keys[i]->KeyVal, key) == 0) {
-		return vals[i];
+	if (i <= N && strcmp(keys[i].KeyVal, key) == 0) {
+		return &vals[i];
 	}
 	return NULL;
 }
 
 void Put(char * key, int val) {
 	int i = Rank(key);
-	if (i < N && strcmp(keys[i]->KeyVal, key) == 0) {
-		vals[i]->ValVal = val;
+	if (i < N && strcmp(keys[i].KeyVal, key) == 0) {
+		vals[i].ValVal = val;
 		return;
 	}
 
 	for (int j = N; j > i; j--) {
-		*keys[j] = *keys[j-1];
-		*vals[j] = *vals[j-1];
+		keys[j] = keys[j-1];
+		vals[j] = vals[j-1];
 	}
-	keys[i] -> KeyVal = strdup(key);
-	vals[i]->ValVal = val;
+	keys[i].KeyVal = strdup(key);
+	vals[i].ValVal = val;
 	N++;
 }
 
 void Show() {
 	for (int i = 0; i < N; i++) {
-		printf("key: %s, val: %d\n", keys[i]->KeyVal, vals[i]->ValVal);
+		printf("key: %s, val: %d\n", keys[i].KeyVal, vals[i].ValVal);
 	}
 }
 
