ecefe9ea886b33d39277c4d89c29baa9c9ab9fe9
avimitin <avimitin@gmail.com>
2020-10-27 17:38:03 +0800

    add find min node and floor method

--- Algorithms/Search/SymbolTable/BST.c
+++ Algorithms/Search/SymbolTable/BST.c
@@ -61,3 +61,41 @@ node * put(node *x, char *key, int val) {
 	x->N = size(x->left) + size(x->right) + 1;
 	return x;
 }
+
+// Delete min using circulation
+node * DelMinCir() {
+	if (root->left == NULL) {
+		return root;
+	}
+	node *x = root;
+	while (x->left != NULL) {
+		x=x->left;
+	}
+	return x;
+}
+
+node * DelMinRec(node *x) {
+	if (x->left == NULL) {
+		return x;
+	} else {
+		return DelMinRec(x);
+	}
+}
+
+char * Min() {
+	return DelMinRec(root)->key->KeyVal;
+}
+
+node * Floor(node *x, char *key) {
+	if (x == NULL) {
+		return x;
+	}
+	int cmp = strcmp(key, x->key->KeyVal);
+	if (cmp < 0) {
+		Floor(x->left, key);
+	} else if (cmp > 0) {
+		Floor(x->right, key);
+	} else {
+		return x;
+	}
+}
